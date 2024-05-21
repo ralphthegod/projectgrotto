0139e4088c0384f044ab72ed17eac9e5418f9f62
RalphTheCreator <rafdimaso@gmail.com>
2024-05-22 01:29:45 +0200

    feat: Update GrottoRenderSystem to emit CurrentViewEvent

--- app/src/main/java/com/deemaso/grotto/systems/GrottoRenderSystem.java
+++ app/src/main/java/com/deemaso/grotto/systems/GrottoRenderSystem.java
@@ -4,12 +4,16 @@ import com.deemaso.core.Box;
 import com.deemaso.core.Entity;
 import com.deemaso.core.GameWorld;
 import com.deemaso.core.components.RenderComponent;
+import com.deemaso.core.events.Event;
+import com.deemaso.core.events.EventEmitter;
+import com.deemaso.core.events.EventListener;
 import com.deemaso.core.systems.RenderSystem;
 import com.deemaso.grotto.GrottoGameWorld;
 import com.deemaso.grotto.components.CameraComponent;
 import com.deemaso.grotto.components.GrottoRenderComponent;
 import com.deemaso.grotto.components.PhysicsComponent;
 import com.deemaso.grotto.data.ResourceLoader;
+import com.deemaso.grotto.events.CurrentViewEvent;
 import com.deemaso.grotto.utils.RenderUtils;
 import com.google.fpl.liquidfun.Body;
 
@@ -21,13 +25,16 @@ import android.graphics.Paint;
 import android.util.Log;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /*
     * Manages rendering of entities in the Grotto game.
     * It uses Canvas and Bitmap from Android SDK to draw entities.
     * It also uses the PhysicsComponent to determine the position of entities.
  */
-public class GrottoRenderSystem extends RenderSystem{
+public class GrottoRenderSystem extends RenderSystem implements EventEmitter {
 
     private final Canvas canvas;
     private final Bitmap buffer;
@@ -96,7 +103,7 @@ public class GrottoRenderSystem extends RenderSystem{
     public boolean draw(Entity entity, float deltaTime) {
         RenderComponent render = entity.getComponent(GrottoRenderComponent.class);
         PhysicsComponent physics = entity.getComponent(PhysicsComponent.class);
-        // Only last camera component is considered
+        // Only last created camera component is considered
         CameraComponent camera = entity.getComponent(CameraComponent.class);
 
         final Body body = physics.getBody();
@@ -115,6 +122,9 @@ public class GrottoRenderSystem extends RenderSystem{
                 currentView.ymax = y + currentView.height / 2 / zoom;
             }
 
+            // Emit the current view event, so that other systems can use it if needed
+            emitEvent(new CurrentViewEvent(currentView));
+
             if(x > currentView.xmin && x < currentView.xmax &&
                     y > currentView.ymin && y < currentView.ymax) {
                 float screen_x = RenderUtils.toPixelsX(x, currentView.xmin, currentView.width, buffer.getWidth());
@@ -130,13 +140,13 @@ public class GrottoRenderSystem extends RenderSystem{
         return false;
     }
 
-    public void draw(RenderComponent render, float x, float y, float angle, float velocityX, float velocityY, float deltaTime) {
+    private void draw(RenderComponent render, float x, float y, float angle, float velocityX, float velocityY, float deltaTime) {
         GrottoRenderComponent grottoRender = (GrottoRenderComponent) render;
         grottoRender.getCanvas().save();
         grottoRender.getCanvas().rotate((float) Math.toDegrees(angle), x, y);
         Bitmap nextFrame = grottoRender.getNextFrame(deltaTime);
 
-        // Flip the bitmap if the entity is moving to the left
+        // Flip the bitmap if the entity is moving to the left (do it only if the entity has a related component)
         if (velocityX < 0) {
             Matrix flipHorizontalMatrix = new Matrix();
             flipHorizontalMatrix.setScale(-1, 1);
@@ -158,4 +168,20 @@ public class GrottoRenderSystem extends RenderSystem{
         return buffer;
     }
 
+    final private static Map<Class<? extends Event>, List<EventListener>> listeners = new HashMap<>();
+
+    @Override
+    public Map<Class<? extends Event>, List<EventListener>> getListeners() {
+        return listeners;
+    }
+
+    @Override
+    public <T extends Event> void emitEvent(T event) {
+        EventEmitter.super.emitEvent(event);
+    }
+
+    @Override
+    public <T extends Event> void registerListener(Class<T> eventType, EventListener<T> listener) {
+        EventEmitter.super.registerListener(eventType, listener);
+    }
 }
