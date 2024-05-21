6226a99f35ad91b3ed4398b5369482823c726ab7
RalphTheCreator <rafdimaso@gmail.com>
2024-05-22 01:29:19 +0200

    refactor: Add UIManager to GrottoGameWorld for UI management

--- app/src/main/java/com/deemaso/grotto/GrottoGameWorld.java
+++ app/src/main/java/com/deemaso/grotto/GrottoGameWorld.java
@@ -6,6 +6,7 @@ import com.deemaso.core.GameWorld;
 import com.deemaso.core.EntityManager;
 import com.deemaso.core.systems.System;
 import com.deemaso.grotto.data.ResourceLoader;
+import com.deemaso.grotto.ui.UIManager;
 
 import java.util.List;
 
@@ -13,6 +14,7 @@ public class GrottoGameWorld extends GameWorld{
 
     final private Activity activity;
     final private ResourceLoader resourceLoader;
+    private UIManager uiManager;
 
     public GrottoGameWorld(EntityManager entityManager, Activity activity) {
         super(entityManager);
@@ -20,6 +22,21 @@ public class GrottoGameWorld extends GameWorld{
         this.resourceLoader = new ResourceLoader(activity.getResources());
     }
 
+    public void setUIManager(UIManager uiManager) {
+        this.uiManager = uiManager;
+    }
+
+    public UIManager getUIManager() {
+        return uiManager;
+    }
+
+    @Override
+    public void update(float dt) {
+        super.update(dt);
+        if(uiManager != null) {
+            uiManager.draw();
+        }
+    }
 
     public Activity getActivity() {
         return activity;
