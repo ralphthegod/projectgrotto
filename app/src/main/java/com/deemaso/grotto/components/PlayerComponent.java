f92c0b2f6fa3c6e6c3213e8d057bda3b1d63b84f
RalphTheCreator <rafdimaso@gmail.com>
2024-05-25 11:34:36 +0200

    feat: Add PlayerComponent as a marker component for player entities

---
+++ app/src/main/java/com/deemaso/grotto/components/PlayerComponent.java
@@ -0,0 +1,7 @@
+package com.deemaso.grotto.components;
+
+import com.deemaso.core.components.Component;
+
+public class PlayerComponent extends Component{
+    // Marker component for player entities
+}
