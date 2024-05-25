a17d63485ff5ca672aa5aa69c146055fd06b38a9
RalphTheCreator <rafdimaso@gmail.com>
2024-05-25 11:36:25 +0200

    refactor: Update GrottoCollisionSystem to use JBox2D library

--- app/src/main/java/com/deemaso/grotto/systems/GrottoCollisionSystem.java
+++ app/src/main/java/com/deemaso/grotto/systems/GrottoCollisionSystem.java
@@ -1,18 +1,29 @@
 package com.deemaso.grotto.systems;
 
+import android.util.Log;
+
 import com.deemaso.core.GameWorld;
 import com.deemaso.core.collisions.Collision;
+import com.deemaso.core.events.Event;
+import com.deemaso.core.events.EventEmitter;
+import com.deemaso.core.events.EventListener;
 import com.deemaso.core.systems.CollisionSystem;
 import com.deemaso.grotto.components.PhysicsComponent;
+import com.deemaso.grotto.events.CollisionEvent;
 import com.deemaso.grotto.listeners.CollisionListener;
-import com.google.fpl.liquidfun.ContactListener;
+
+import org.jbox2d.callbacks.ContactListener;
+import org.jbox2d.dynamics.contacts.Contact;
 
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
+import java.util.Map;
 
-public class GrottoCollisionSystem extends CollisionSystem {
+public class GrottoCollisionSystem extends CollisionSystem implements EventEmitter {
 
     private final ContactListener contactListener;
+    private final Map<Class<? extends Event>, List<EventListener>> listeners = new java.util.HashMap<>();
 
     public GrottoCollisionSystem(GameWorld gameWorld) {
         super(gameWorld, Arrays.asList(PhysicsComponent.class));
@@ -31,7 +42,13 @@ public class GrottoCollisionSystem extends CollisionSystem {
 
     @Override
     protected void handleCollision(Collision collision) {
-        // TO DO: handle collision
+        Log.d("CollisionSystem", "Collision detected");
+        emitEvent(new CollisionEvent(collision));
+
+    }
+
+    public ContactListener getContactListener() {
+        return contactListener;
     }
 
     @Override
@@ -47,5 +64,20 @@ public class GrottoCollisionSystem extends CollisionSystem {
     @Override
     public void resume() {
 
+    }
+
+    @Override
+    public Map<Class<? extends Event>, List<EventListener>> getListeners() {
+        return listeners;
+    }
+
+    @Override
+    public <T extends Event> void emitEvent(T event) {
+        EventEmitter.super.emitEvent(event);
+    }
+
+    @Override
+    public <T extends Event> void registerListener(Class<T> eventType, EventListener<T> listener) {
+        EventEmitter.super.registerListener(eventType, listener);
     }
 }
