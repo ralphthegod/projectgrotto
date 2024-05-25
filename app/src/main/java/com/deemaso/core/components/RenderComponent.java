ebb43e9409beed2b57716eddb8d1d63b366b9480
RalphTheCreator <rafdimaso@gmail.com>
2024-05-25 11:32:47 +0200

    refactor: Add x, y, and zIndex properties to RenderComponent

--- app/src/main/java/com/deemaso/core/components/RenderComponent.java
+++ app/src/main/java/com/deemaso/core/components/RenderComponent.java
@@ -2,8 +2,13 @@ package com.deemaso.core.components;
 
 public class RenderComponent extends Component {
 
+    private float x;
+    private float y;
+
     private float width;
     private float height;
+
+    private int zIndex;
 
     public float getHeight() {
         return height;
@@ -19,5 +24,29 @@ public class RenderComponent extends Component {
 
     public void setHeight(float height) {
         this.height = height;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public int getZIndex() {
+        return zIndex;
+    }
+
+    public void setZIndex(int zIndex) {
+        this.zIndex = zIndex;
     }
 }
