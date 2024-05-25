780bb68e921180230d18c18df34705d6c78938ad
RalphTheCreator <rafdimaso@gmail.com>
2024-05-25 11:34:50 +0200

    refactor: Improve resource loading efficiency and caching

--- app/src/main/java/com/deemaso/grotto/data/ResourceLoader.java
+++ app/src/main/java/com/deemaso/grotto/data/ResourceLoader.java
@@ -5,12 +5,19 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Typeface;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ResourceLoader {
     private final Resources resources;
     private final BitmapFactory.Options options;
+
+    // Cache bitmaps to avoid decoding them multiple times
+    private final Map<String, Bitmap> bitmaps = new HashMap<>();
+    private final Map<String, Typeface> fonts = new HashMap<>();
 
     public ResourceLoader(Resources resources) {
         this.resources = resources;
@@ -19,7 +26,38 @@ public class ResourceLoader {
     }
 
     public Bitmap loadBitmap(int resourceId) {
-        return BitmapFactory.decodeResource(resources, resourceId, options);
+        String key = String.valueOf(resourceId);
+        if (bitmaps.containsKey(key)) {
+            return bitmaps.get(key);
+        }
+        else{
+            Bitmap bitmap = BitmapFactory.decodeResource(resources, resourceId, options);
+            bitmaps.put(key, bitmap);
+            return bitmap;
+        }
+    }
+
+    public Bitmap loadBitmapAsset(String asset) {
+        if (bitmaps.containsKey(asset)) {
+            return bitmaps.get(asset);
+        }
+
+        try {
+            Bitmap bitmap = BitmapFactory.decodeStream(resources.getAssets().open(asset), null, options);
+            bitmaps.put(asset, bitmap);
+            return bitmap;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public List<Bitmap> loadBitmapAssets(List<String> assets) {
+        List<Bitmap> bitmaps = new ArrayList<>();
+        for (String asset : assets) {
+            bitmaps.add(loadBitmapAsset(asset));
+        }
+        return bitmaps;
     }
 
     public List<Bitmap> loadBitmaps(List<Integer> resourceIds) {
@@ -31,6 +69,11 @@ public class ResourceLoader {
     }
 
     public Typeface loadFont(String resource) {
-        return Typeface.createFromAsset(resources.getAssets(), resource);
+        if (fonts.containsKey(resource)) {
+            return fonts.get(resource);
+        }
+        Typeface font = Typeface.createFromAsset(resources.getAssets(), resource);
+        fonts.put(resource, font);
+        return font;
     }
 }
