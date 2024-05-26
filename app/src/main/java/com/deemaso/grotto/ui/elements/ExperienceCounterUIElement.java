4d5bccd660c9468ce50b8057a3034edc821ee028
RalphTheCreator <rafdimaso@gmail.com>
2024-05-26 22:42:00 +0200

    feat: Add ExperienceCounterUIElement for displaying player's experience

---
+++ app/src/main/java/com/deemaso/grotto/ui/elements/ExperienceCounterUIElement.java
@@ -0,0 +1,38 @@
+package com.deemaso.grotto.ui.elements;
+
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.util.Log;
+
+import com.deemaso.core.events.EventListener;
+import com.deemaso.core.events.SystemEvent;
+import com.deemaso.grotto.ui.UIElement;
+
+public class ExperienceCounterUIElement extends UIElement implements EventListener {
+
+    public ExperienceCounterUIElement(float x, float y, float width, float height, Bitmap image, int textSize, Typeface typeface) {
+        super(x, y, width, height);
+        TextUIElement textUIElement = new TextUIElement(x + 40, y, width, height, "0/10", textSize, typeface, Color.WHITE);
+        ImageUIElement imageUIElement = new ImageUIElement(x, y - 25, width, height, image);
+        children.add(textUIElement);
+        children.add(imageUIElement);
+    }
+
+    @Override
+    public void draw(float screenX, float screenY) {
+        super.draw(screenX, screenY);
+    }
+
+    @Override
+    public void onEvent(SystemEvent event) {
+        if(event.getCode().equals("PLAYER_EXPERIENCE_UPDATED"))
+        {
+            for (UIElement child : children) {
+                if(child instanceof TextUIElement){
+                    ((TextUIElement) child).setText(event.get("experience") + "/10");
+                }
+            }
+        }
+    }
+}
