630d6b82bf5eac25335017bac6c7f6dca6e2ca85
RalphTheCreator <rafdimaso@gmail.com>
2024-05-28 17:08:59 +0200

    feat: Add MovementComponent and MovementSystem

---
+++ app/src/main/java/com/deemaso/grotto/components/MovementComponent.java
@@ -0,0 +1,68 @@
+package com.deemaso.grotto.components;
+
+import com.deemaso.core.components.Component;
+
+import java.util.Queue;
+import java.util.LinkedList;
+
+public class MovementComponent extends Component {
+
+    private float power;
+    private float speed;
+    private long lastMovementStartTime;
+
+    public long getLastMovementStartTime() {
+        return lastMovementStartTime;
+    }
+
+    public void setLastMovementStartTime(long lastMovementStartTime) {
+        this.lastMovementStartTime = lastMovementStartTime;
+    }
+
+    static public class Movement {
+        public float directionX, directionY;
+
+        public Movement(float directionX, float directionY, float power) {
+            this.directionX = directionX;
+            this.directionY = directionY;
+        }
+    }
+
+    private final Queue<Movement> movementQueue;
+
+    public MovementComponent(float power, float speed) {
+        this.power = power;
+        this.speed = speed;
+        movementQueue = new LinkedList<>();
+    }
+
+    public void addMovement(float directionX, float directionY, float power) {
+        movementQueue.add(new Movement(directionX, directionY, power));
+    }
+
+    public Movement getNextMovement() {
+        return movementQueue.poll();
+    }
+
+    public boolean hasMovements() {
+        return !movementQueue.isEmpty();
+    }
+
+    public float getPower() {
+        return power;
+    }
+
+    public void setPower(float power) {
+        this.power = power;
+    }
+
+    public float getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(float speed) {
+        this.speed = speed;
+    }
+
+
+}
---
+++ app/src/main/java/com/deemaso/grotto/systems/MovementSystem.java
@@ -0,0 +1,88 @@
+package com.deemaso.grotto.systems;
+
+import com.deemaso.core.Entity;
+import com.deemaso.core.GameWorld;
+import com.deemaso.core.events.SystemEvent;
+import com.deemaso.core.systems.System;
+import com.deemaso.grotto.components.MovementComponent;
+import com.deemaso.grotto.components.PhysicsComponent;
+
+import org.jbox2d.common.Vec2;
+
+import java.util.Arrays;
+
+public class MovementSystem extends System {
+
+    public MovementSystem(GameWorld gameWorld) {
+        super(gameWorld, Arrays.asList(MovementComponent.class, PhysicsComponent.class), true);
+    }
+
+    @Override
+    public void update(float dt) {
+        super.update(dt);
+
+        for (Entity entity : entities) {
+            MovementComponent movement = entity.getComponent(MovementComponent.class);
+            PhysicsComponent physics = entity.getComponent(PhysicsComponent.class);
+
+            if (movement != null && physics != null) {
+                long currentTime = java.lang.System.currentTimeMillis();
+                long timeSinceLastMovement = currentTime - movement.getLastMovementStartTime();
+
+                if (movement.hasMovements() && timeSinceLastMovement >= (1000 / movement.getSpeed())) {
+                    MovementComponent.Movement nextMovement = movement.getNextMovement();
+                    if (nextMovement != null) {
+                        applyImpulse(physics, nextMovement, movement.getPower());
+                        movement.setLastMovementStartTime(currentTime);
+                    }
+                } else if (!movement.hasMovements() && timeSinceLastMovement >= (1000 / movement.getSpeed())) {
+                    stopMovement(physics);
+                    movement.setLastMovementStartTime(currentTime);
+                }
+            }
+        }
+    }
+
+    @Override
+    protected void finalize() {
+
+    }
+
+    private void applyImpulse(PhysicsComponent physics, MovementComponent.Movement movement, float power) {
+        Vec2 direction = new Vec2(movement.directionX, movement.directionY);
+        direction.normalize();
+        Vec2 impulse = direction.mul(power);
+
+        physics.getBody().applyLinearImpulse(impulse, physics.getBody().getWorldCenter());
+    }
+
+    private void stopMovement(PhysicsComponent physics) {
+        physics.getBody().setLinearVelocity(new Vec2(0, 0));
+        physics.getBody().setAngularVelocity(0);
+    }
+
+    @Override
+    public boolean registerEntity(Entity entity) {
+        return super.registerEntity(entity);
+    }
+
+    @Override
+    public void unregisterEntity(Entity entity) {
+        super.unregisterEntity(entity);
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void onEvent(SystemEvent event) {
+
+    }
+}
