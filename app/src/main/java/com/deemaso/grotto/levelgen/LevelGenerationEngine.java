4f2c1c943fd0cd754834ccbc9ea50e0bea8be261
RalphTheCreator <rafdimaso@gmail.com>
2024-05-29 15:32:57 +0200

    feat: Improve tunnel creation algorithm in LevelGenerationEngine

--- app/src/main/java/com/deemaso/grotto/levelgen/LevelGenerationEngine.java
+++ app/src/main/java/com/deemaso/grotto/levelgen/LevelGenerationEngine.java
@@ -123,7 +123,7 @@ public class LevelGenerationEngine {
      */
     private void createHTunnel(int x1, int x2, int y) {
         for (int x = Math.min(x1, x2); x <= Math.max(x1, x2); x++) {
-            for (int i = 0; i < 3; i++) {
+            for (int i = 0; i < 4; i++) {
                 if (isInBounds(y + i, x)) {
                     grid[y + i][x] = FLOOR;
                 }
@@ -140,7 +140,7 @@ public class LevelGenerationEngine {
      */
     private void createVTunnel(int y1, int y2, int x) {
         for (int y = Math.min(y1, y2); y <= Math.max(y1, y2); y++) {
-            for (int i = 0; i < 3; i++) {
+            for (int i = 0; i < 4; i++) {
                 if (isInBounds(y, x + i)) {
                     grid[y][x + i] = FLOOR;
                 }
