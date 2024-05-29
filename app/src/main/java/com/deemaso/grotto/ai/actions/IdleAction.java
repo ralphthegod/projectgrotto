7137907ddf05d931083b1909c66f7aec26577d04
RalphTheCreator <rafdimaso@gmail.com>
2024-05-29 15:32:35 +0200

    feat: Add IdleAction class for idle behavior

---
+++ app/src/main/java/com/deemaso/grotto/ai/actions/IdleAction.java
@@ -0,0 +1,11 @@
+package com.deemaso.grotto.ai.actions;
+
+import com.deemaso.grotto.ai.AIContext;
+import com.deemaso.grotto.ai.Action;
+
+public class IdleAction implements Action {
+    @Override
+    public void execute(AIContext context) {
+        // Do nothing
+    }
+}
