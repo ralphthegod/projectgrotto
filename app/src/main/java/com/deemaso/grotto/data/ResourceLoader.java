89f1e02643060c92e31559a561b84c0f99c1bbad
RalphTheCreator <rafdimaso@gmail.com>
2024-06-04 00:54:52 +0200

    refactor: Add loadWeapon method to ResourceLoader

--- app/src/main/java/com/deemaso/grotto/data/ResourceLoader.java
+++ app/src/main/java/com/deemaso/grotto/data/ResourceLoader.java
@@ -1,17 +1,29 @@
 package com.deemaso.grotto.data;
 
+import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Typeface;
+import android.util.Log;
 
+import com.deemaso.grotto.items.MeleeWeapon;
+import com.deemaso.grotto.items.RangedWeapon;
+import com.deemaso.grotto.items.Weapon;
+import com.deemaso.grotto.utils.Helpers;
 import com.deemaso.grotto.utils.RenderUtils;
 
+import org.w3c.dom.Document;
+
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 
 public class ResourceLoader {
     private final Resources resources;
@@ -77,5 +89,36 @@ public class ResourceLoader {
         Typeface font = Typeface.createFromAsset(resources.getAssets(), resource);
         fonts.put(resource, font);
         return font;
+    }
+
+    public static Weapon loadWeapon(Context context, String path) {
+        try {
+            InputStream inputStream = context.getAssets().open("archetypes/weapons/" + path + ".xml");
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(inputStream);
+            doc.getDocumentElement().normalize();
+
+            String name = Helpers.getAttributeAsString(doc.getDocumentElement(), "weaponName", "");
+            String archetype = Helpers.getAttributeAsString(doc.getDocumentElement(), "archetype", "");
+            int damage =  Helpers.getAttributeAsInt(doc.getDocumentElement(), "damage", 0);
+            float knockback = Helpers.getAttributeAsFloat(doc.getDocumentElement(), "knockback", 0);
+            String type = Helpers.getAttributeAsString(doc.getDocumentElement(), "type", "");
+
+            if(type.equals("MELEE")){
+                float slashSpeed = Helpers.getAttributeAsFloat(doc.getDocumentElement(), "slashSpeed", 0);
+                return new MeleeWeapon(name, archetype, damage, slashSpeed, knockback);
+            }
+            else if(type.equals("RANGED")){
+                float bulletSpeed = Helpers.getAttributeAsFloat(doc.getDocumentElement(), "bulletSpeed", 0);
+                return new RangedWeapon(name, archetype, damage, bulletSpeed, knockback);
+            }
+            else{
+                return new Weapon(name, archetype, damage, knockback);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
