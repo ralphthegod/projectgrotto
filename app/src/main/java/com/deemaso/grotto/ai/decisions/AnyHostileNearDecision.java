cfcd288dbcd87deb990a3dc398b59bbc47e5db11
RalphTheCreator <rafdimaso@gmail.com>
2024-06-05 07:50:24 +0200

    refactor: use getStat method instead of deprecated getFaction

--- app/src/main/java/com/deemaso/grotto/ai/decisions/AnyHostileNearDecision.java
+++ app/src/main/java/com/deemaso/grotto/ai/decisions/AnyHostileNearDecision.java
@@ -6,7 +6,10 @@ import com.deemaso.grotto.ai.Decision;
 import com.deemaso.grotto.components.CharacterStatsComponent;
 import com.deemaso.grotto.components.PerceptionComponent;
 
+import org.w3c.dom.Element;
+
 public class AnyHostileNearDecision implements Decision {
+
     @Override
     public boolean evaluate(AIContext context) {
         Entity self = (Entity) context.get("self");
@@ -15,7 +18,7 @@ public class AnyHostileNearDecision implements Decision {
             for(Entity entity: perceptionComponent.getPerceivedEntities()){
                 if(entity.hasComponent(CharacterStatsComponent.class)){
                     CharacterStatsComponent characterStatsComponent = entity.getComponent(CharacterStatsComponent.class);
-                    if(!characterStatsComponent.getFaction().equals(self.getComponent(CharacterStatsComponent.class).getFaction())){
+                    if(!characterStatsComponent.getStat("faction").equals(self.getComponent(CharacterStatsComponent.class).getStat("faction"))){
                         context.remove("target");
                         context.put("target", entity);
                         return true;
