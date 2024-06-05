134e59340dac90e494a1215a9fb785e68c4aea76
RalphTheCreator <rafdimaso@gmail.com>
2024-06-05 07:51:34 +0200

    refactor: migrate to more generalized map-based character stats

--- app/src/main/java/com/deemaso/grotto/components/CharacterStatsComponent.java
+++ app/src/main/java/com/deemaso/grotto/components/CharacterStatsComponent.java
@@ -2,42 +2,30 @@ package com.deemaso.grotto.components;
 
 import com.deemaso.core.components.Component;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CharacterStatsComponent extends Component {
-    private int level = 1;
-    private int experience = 0;
-    private int maxHealth = 100;
-    private int health = 100;
     private boolean isAlive = true;
-    private String faction;
+    private final Map<String, Object> stats = new HashMap<>();
 
     public CharacterStatsComponent(int experience, String faction, int maxHealth) {
-        this.experience = experience;
-        this.faction = faction;
-        this.maxHealth = maxHealth;
-        this.health = maxHealth;
+        stats.put("experience", experience);
+        stats.put("level", 1);
+        stats.put("faction", faction);
+        stats.put("maxHealth", maxHealth);
+        stats.put("health", maxHealth);
+    }
+
+    public Object getStat(String key) {
+        return stats.get(key);
+    }
+
+    public void setStat(String key, Object value) {
+        stats.put(key, value);
     }
 
     public CharacterStatsComponent() {
-    }
-
-    public void setLevel(int level) {
-        this.level = level;
-    }
-
-    public int getLevel() {
-        return level;
-    }
-
-    public int getExperience() {
-        return experience;
-    }
-
-    public void setExperience(int experience) {
-        this.experience = experience;
-    }
-
-    public void addExperience(int experience) {
-        this.experience += experience;
     }
 
     public boolean isAlive() {
@@ -48,27 +36,4 @@ public class CharacterStatsComponent extends Component {
         isAlive = alive;
     }
 
-    public String getFaction() {
-        return faction;
-    }
-
-    public void setFaction(String faction) {
-        this.faction = faction;
-    }
-
-    public int getMaxHealth() {
-        return maxHealth;
-    }
-
-    public void setMaxHealth(int maxHealth) {
-        this.maxHealth = maxHealth;
-    }
-
-    public int getHealth() {
-        return health;
-    }
-
-    public void setHealth(int health) {
-        this.health = health;
-    }
 }
