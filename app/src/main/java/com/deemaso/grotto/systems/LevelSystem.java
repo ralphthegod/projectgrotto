5d0736f61cf3fdb81a220d3e9dfd39dcfa13447d
RalphTheCreator <rafdimaso@gmail.com>
2024-06-09 20:34:01 +0200

    refactor: handle new levels

--- app/src/main/java/com/deemaso/grotto/systems/LevelSystem.java
+++ app/src/main/java/com/deemaso/grotto/systems/LevelSystem.java
@@ -1,25 +1,37 @@
 package com.deemaso.grotto.systems;
 
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.util.Log;
+
+import androidx.annotation.NonNull;
 
 import com.deemaso.core.Entity;
 import com.deemaso.core.GameWorld;
+import com.deemaso.core.collisions.Collision;
 import com.deemaso.core.components.Component;
 import com.deemaso.core.events.SystemEvent;
 import com.deemaso.core.systems.System;
+import com.deemaso.grotto.GrottoGameWorld;
+import com.deemaso.grotto.components.CharacterStatsComponent;
+import com.deemaso.grotto.components.EnemySpawnerComponent;
 import com.deemaso.grotto.components.GrottoRenderComponent;
 import com.deemaso.grotto.components.LevelDefinitionComponent;
 import com.deemaso.grotto.components.PhysicsComponent;
+import com.deemaso.grotto.components.PlayerComponent;
 import com.deemaso.grotto.components.TileComponent;
+import com.deemaso.grotto.levelgen.EnemyElementDefinition;
 import com.deemaso.grotto.levelgen.LevelGenerationElementDefinition;
 import com.deemaso.grotto.levelgen.LevelGenerationEngine;
 
 import org.jbox2d.common.Vec2;
 
+import java.io.Serializable;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 /**
  *  The LevelSystem is responsible for generating the level based on the LevelDefinitionComponent
@@ -33,7 +45,10 @@ public class LevelSystem extends System{
     private final Map<String, Entity> tileMap;
     private final Map<String, Entity> entityMap;
     private boolean isGenerating = false;
+    private boolean nextLevel = false;
     private final LevelGenerationEngine levelGenerationEngine;
+    private int level = 0;
+    private PlayerData playerData = null;
     private final Entity levelEntity;
     private final float tileSizeInPixels;
 
@@ -41,11 +56,15 @@ public class LevelSystem extends System{
             GameWorld gameWorld,
             boolean requireAllComponents,
             String levelEntityArchetypeId,
-            float tileSizeInPixels
+            float tileSizeInPixels,
+            PlayerData playerData
     ) {
-        super(gameWorld, Arrays.asList(), requireAllComponents);
+        super(gameWorld, Arrays.asList(PlayerComponent.class), requireAllComponents);
         this.tileSizeInPixels = tileSizeInPixels;
-        Entity levelEntity = gameWorld.createEntityById(levelEntityArchetypeId);
+        this.levelEntity = gameWorld.createEntityById(levelEntityArchetypeId);
+        this.playerData = playerData;
+        tileMap = new HashMap<>();
+        entityMap = new HashMap<>();
         LevelDefinitionComponent levelDefinitionComponent = levelEntity.getComponent(LevelDefinitionComponent.class);
         this.levelGenerationEngine = new LevelGenerationEngine(
                 levelDefinitionComponent.getGridWidth(),
@@ -56,21 +75,67 @@ public class LevelSystem extends System{
                 levelDefinitionComponent.getLevelGenerationElementDefinitions(),
                 (int) (Math.random() * 1000000)
         );
-        this.levelEntity = levelEntity;
-        tileMap = new HashMap<>();
-        entityMap = new HashMap<>();
+    }
+
+    /*
+    * Used to transport player data during level transition
+    * */
+    public static class PlayerData implements Serializable {
+        final private Map<String, Object> stats;
+        final private int level;
+
+        private PlayerData(
+                Map<String, Object> stats,
+                int level
+        ) {
+            this.stats = stats;
+            this.level = level;
+        }
+
+        public Map<String, Object> getStats() {
+            return stats;
+        }
+
+        public int getLevel() {
+            return level;
+        }
+
+    }
+
+    public void generateNextLevel(){
+        CharacterStatsComponent pc = null;
+        for(Entity e : entities){
+            if(e.hasComponent(PlayerComponent.class)){
+                Log.d("LevelSystem", "PlayerData found: loading stats...");
+                pc = e.getComponent(CharacterStatsComponent.class);
+            }
+        }
+        if(pc != null) playerData = new PlayerData(pc.getStats(), level++);
+        ((GrottoGameWorld) gameWorld).restart(playerData);
     }
 
     @Override
     public void update(float dt) {
-        checkLevelGeneration();
+        if(nextLevel){
+            generateNextLevel();
+        }
+        else if(checkLevelGeneration()){
+            Log.d("LevelSystem", "Level generation finished.");
+            Entity pc = null;
+            for(Entity e : entities){
+                if(e.hasComponent(PlayerComponent.class)){
+                    Log.d("LevelSystem", "Saving player entity...");
+                    pc = e;
+                }
+            }
+        }
     }
 
     /**
      *  Checks if the level is being generated and if the tile map is empty. If the level is not being
      *  generated and the tile map is empty, then it generates the level.
      * */
-    private void checkLevelGeneration(){
+    private boolean checkLevelGeneration(){
         if(!isGenerating && tileMap.isEmpty()) {
             boolean success = false;
             int attempts = 0;
@@ -78,12 +143,14 @@ public class LevelSystem extends System{
                 try {
                     success = generate();
                 } catch(Exception e) {
-                    Log.d("LevelSystem", "Error generating level. Retrying...");
+                    Log.e("LevelSystem", "Error generating level. Retrying...");
                     e.printStackTrace();
                     attempts++;
                 }
             }
+            return success;
         }
+        return false;
     }
 
     @Override
@@ -154,6 +221,12 @@ public class LevelSystem extends System{
                         addEntity(x, y, tileEntity);
                         addBaseTile(x, y, tile);
                     }
+                    if(tileEntity.hasComponent(PlayerComponent.class) && playerData != null){
+                        tileEntity.getComponent(CharacterStatsComponent.class).setStats(playerData.getStats());
+                        SystemEvent event = new SystemEvent("PLAYER_LOADED");
+                        event.put("entity", tileEntity);
+                        gameWorld.broadcastEvent(event);
+                    }
                 }
             }
         }
@@ -211,6 +284,7 @@ public class LevelSystem extends System{
      * */
     private boolean generate() {
         isGenerating = true;
+
         LevelDefinitionComponent levDefComp = levelEntity.getComponent(LevelDefinitionComponent.class);
         Map<Character, LevelGenerationElementDefinition> levelGenerationElementDefinitions = new HashMap<>();
         for(LevelGenerationElementDefinition def : levDefComp.getLevelGenerationElementDefinitions()) {
@@ -243,9 +317,11 @@ public class LevelSystem extends System{
             Log.d("LevelSystem", "Level generation failed. Level is empty.");
             return false;
         }
+
+        Log.d("LevelSystem", "PhysicsWorld bodycount before level generation: " + ( (PhysicsSystem) gameWorld.getSystem(PhysicsSystem.class)).physicsWorld.getBodyCount());
+
         generatedDungeonToTiles(level, levelGenerationElementDefinitions);
 
-        // Set the position of the tiles
         for(Map.Entry<String, Entity> entry : tileMap.entrySet()) {
             String[] coordinates = entry.getKey().split(",");
             int x = Integer.parseInt(coordinates[0]);
@@ -253,19 +329,110 @@ public class LevelSystem extends System{
             setTilesPosition(entry.getValue(), x, y);
         }
 
-        // Set the position of the entities
         for(Map.Entry<String, Entity> entry : entityMap.entrySet()) {
             String[] coordinates = entry.getKey().split(",");
             int x = Integer.parseInt(coordinates[0]);
             int y = Integer.parseInt(coordinates[1]);
             setTilesPosition(entry.getValue(), x, y);
         }
+
+        generateEnemies();
+
         isGenerating = false;
         return true;
     }
 
+    private void generateEnemies() {
+        final Map<String, Integer> enemyCountMap = new HashMap<>();
+        LevelDefinitionComponent levelDefinitionComponent = levelEntity.getComponent(LevelDefinitionComponent.class);
+        int playerLevel = 1;
+
+        if(playerData != null){
+            playerLevel = (int) playerData.getStats().get("level");
+        }
+
+        for (Entity entity : entityMap.values()){
+            if (entity.hasComponent(EnemySpawnerComponent.class)) {
+                EnemyElementDefinition selectedEnemyDef = getWeightedRandomEnemyDefinition(
+                        levelDefinitionComponent.getEnemyElementDefinitions(),
+                        playerLevel,
+                        enemyCountMap
+                );
+                if (selectedEnemyDef != null) {
+                    Entity enemy = gameWorld.createEntityById(selectedEnemyDef.getEnemyArchetypeId());
+
+                    PhysicsComponent spawnerPhysics = entity.getComponent(PhysicsComponent.class);
+                    if (spawnerPhysics != null) {
+                        PhysicsComponent enemyPhysics = enemy.getComponent(PhysicsComponent.class);
+                        if (enemyPhysics != null) {
+                            enemyPhysics.getBody().setTransform(spawnerPhysics.getBody().getPosition(), 0);
+                        }
+                    }
+
+                    CharacterStatsComponent enemyStats = enemy.getComponent(CharacterStatsComponent.class);
+                    if (enemyStats != null) {
+                        int enemyLevel = calculateEnemyLevel(playerLevel, level);
+                        enemyStats.setStat("level", enemyLevel);
+                    }
+
+                    int currentEnemyCount = enemyCountMap.get(selectedEnemyDef.getEnemyArchetypeId()) == null ? 0 : enemyCountMap.get(selectedEnemyDef.getEnemyArchetypeId());
+                    enemyCountMap.put(selectedEnemyDef.getEnemyArchetypeId(), currentEnemyCount + 1);
+                    Log.d("LevelSystem", "Generated enemy " + selectedEnemyDef.getEnemyArchetypeId() + " (Lvl " + enemyStats.getStat("level") + ") at spawner position");
+                }
+
+                gameWorld.markEntityForDeletion(entity);
+            }
+        }
+    }
+
+    private int calculateEnemyLevel(int playerLevel, int dungeonDepth) {
+        double factor = 0.5;
+        int baseEnemyLevel = (int) Math.max(1, playerLevel + dungeonDepth * factor);
+        Random random = new Random();
+        int levelVariation = random.nextInt(3) - 1;
+        int enemyLevel = Math.max(1, baseEnemyLevel + levelVariation);
+        return enemyLevel;
+    }
+
+    private EnemyElementDefinition getWeightedRandomEnemyDefinition(List<EnemyElementDefinition> enemyElementDefinitions, int currentLevel, Map<String, Integer> enemyCountMap) {
+        final Random random = new Random();
+        float totalWeight = 0.0f;
+        for (EnemyElementDefinition def : enemyElementDefinitions) {
+            if (currentLevel >= def.getMinPlayerLevel() && enemyCountMap.getOrDefault(def.getEnemyArchetypeId(), 0) < def.getMaxQuantity()) {
+                totalWeight += def.getProbability();
+            }
+        }
+
+        float randomValue = random.nextFloat() * totalWeight;
+        for (EnemyElementDefinition def : enemyElementDefinitions) {
+            if (currentLevel >= def.getMinPlayerLevel() && enemyCountMap.getOrDefault(def.getEnemyArchetypeId(), 0) < def.getMaxQuantity()) {
+                randomValue -= def.getProbability();
+                if (randomValue <= 0) {
+                    return def;
+                }
+            }
+        }
+        return null;
+    }
+
     @Override
     public void onEvent(SystemEvent event) {
-
+        if(event.getCode().equals("COLLISION")){
+            Collision collision = (Collision) event.get("collision");
+            Entity entity1 = collision.getA();
+            Entity entity2 = collision.getB();
+            if(entity1.hasComponent(PlayerComponent.class) && entity2.hasComponent(TileComponent.class)){
+                TileComponent tile = entity2.getComponent(TileComponent.class);
+                if(tile.getTag().equals("ending")){
+                    nextLevel = true;
+                }
+            }
+            else if(entity2.hasComponent(PlayerComponent.class) && entity1.hasComponent(TileComponent.class)){
+                TileComponent tile = entity1.getComponent(TileComponent.class);
+                if(tile.getTag().equals("ending")){
+                    nextLevel = true;
+                }
+            }
+        }
     }
 }
