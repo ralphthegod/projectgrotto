e04c73f407dfe03b0808365cfe63e335a7daa6bd
RalphTheCreator <rafdimaso@gmail.com>
2024-06-09 20:29:01 +0200

    refactor: improve SoundComponent with sound management functionality

--- app/src/main/java/com/deemaso/grotto/components/SoundComponent.java
+++ app/src/main/java/com/deemaso/grotto/components/SoundComponent.java
@@ -1,7 +1,32 @@
 package com.deemaso.grotto.components;
 
+import com.badlogic.androidgames.framework.Sound;
 import com.deemaso.core.components.Component;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class SoundComponent extends Component {
- // TODO: Implement SoundComponent
+    private Map<String, Sound> sounds = new HashMap<>();
+    private final Map<String, String> paths;
+
+    public SoundComponent(Map<String, String> paths) {
+        this.paths = paths;
+    }
+
+    public Map<String, Sound> getSounds() {
+        return sounds;
+    }
+
+    public void setSounds(Map<String, Sound> sounds) {
+        this.sounds = sounds;
+    }
+
+    public Map<String, String> getPaths() {
+        return paths;
+    }
+
+    public void addSound(String name, Sound sound) {
+        sounds.put(name, sound);
+    }
 }
