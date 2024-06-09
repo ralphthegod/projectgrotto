354b5a0460b633f5f18ab2e8666aa3560e77a741
RalphTheCreator <rafdimaso@gmail.com>
2024-06-09 20:29:36 +0200

    refactor: add EnemySpawnerComponent to the project

---
+++ app/src/main/java/com/deemaso/grotto/components/EnemySpawnerComponent.java
@@ -0,0 +1,7 @@
+package com.deemaso.grotto.components;
+
+import com.deemaso.core.components.Component;
+
+public class EnemySpawnerComponent extends Component {
+    // Marker component for enemy spawners
+}
